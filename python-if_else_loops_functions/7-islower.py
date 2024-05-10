abc0d9717d5308729ee75f5012ad1ab48f2b98d6
NicowaxDelux <nycko111995@gmail.com>
2024-05-10 10:23:02 -0500

    Write a function that checks for lowercase character.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if chr(97) <= c <= chr(123) and c != '':
+    if chr(97) <= c <= chr(123) and c != chr(''):
         return True
     else:
         return False
