9631a29b89105db5b834e939fcc4b4d3e0c0d731
NicowaxDelux <nycko111995@gmail.com>
2024-05-18 22:42:41 -0500

    Write an empty class Square that defines a square:

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""Square class
+"""Square Class
 
-   This class dont have nothing for the moment
+This class dont have nothing for the moment
 
 """
 
 
 class Square:
+    """class emmpty
+
+    methods for manipulation
+    
+    """
     pass
