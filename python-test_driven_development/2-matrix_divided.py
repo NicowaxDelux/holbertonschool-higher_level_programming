3c681f3a87d61c3036295333d392793cdbe69229
NicowaxDelux <nycko111995@gmail.com>
2024-05-21 17:35:52 -0500

    Write a function that divides all elements of a matrix.

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+
+    new_matrix = []
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_row = []
+        for element in row:
+            if not isinstance(element, int) and not isinstance(element, float):
+                raise TypeError("matrix must be a matrix\
+                                (list of lists) of integers/floats")
+
+            result = round(element / div, 2)
+            new_row.append(result)
+        new_matrix.append(new_row)
+
+    return new_matrix
