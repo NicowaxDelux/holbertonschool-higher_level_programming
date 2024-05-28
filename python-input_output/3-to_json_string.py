0cc0abd4f0ecbfc05ad4a49060ff32ae12664218
NicowaxDelux <nycko111995@gmail.com>
2024-05-27 19:22:05 -0500

    Write a function that returns the JSON representation of an object (string)

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    method json string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """  funtion to json string
+
+        returns the JSON representation of an object
+    """
+    return json.dumps(my_obj)
