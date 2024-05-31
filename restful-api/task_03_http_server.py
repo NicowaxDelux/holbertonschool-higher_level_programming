a1dc8fac090259e9daf263532a10a3fd4de7383a
NicowaxDelux <nycko111995@gmail.com>
2024-05-31 14:56:06 -0500

    Develop a simple API using Python with the  module

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -9,42 +9,26 @@ class SimpleBaseHTTPRequestHandler(BaseHTTPRequestHandler):
         data = {"name": "John", "age": 30, "city": "New York"}
         info = {"version": "1.0", "description": "A simple API built with http.server"}
 
-        if self.path == '/data':
-            try:
+        try:
+            if self.path == '/data':
                 self.send_response(200)
                 self.send_header("Content-Type", "application/json")
                 self.end_headers()
                 response_json = json.dumps(data)
                 self.wfile.write(response_json.encode('utf-8'))
-            except:
-                self.send_response(404)
-                self.send_header("Content-Type", "text/html")
-                self.end_headers()
-                self.wfile.write(b"404 Not Found")
-            finally:
-                return
-
-
-        if self.path == '/info':
-            try:
+                
+            if self.path == '/info':
                 self.send_response(200)
                 self.send_header("Content-Type", "application/json")
                 self.end_headers()
                 response_json = json.dumps(info)
                 self.wfile.write(response_json.encode('utf-8'))
-            except:
-                self.send_response(404)
+
+            if self.path == '/':
+                self.send_response(200)
                 self.send_header("Content-Type", "text/html")
                 self.end_headers()
-                self.wfile.write(b"404 Not Found")
-            finally:
-                return
-
-        try:
-            self.send_response(200)
-            self.send_header("Content-Type", "text/html")
-            self.end_headers()
-            self.wfile.write(b"Hello, this is a simple API!")
+                self.wfile.write(b"Hello, this is a simple API!")
         except:
             self.send_response(404)
             self.send_header("Content-Type", "text/html")
