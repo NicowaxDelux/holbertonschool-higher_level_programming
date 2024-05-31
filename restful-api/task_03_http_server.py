65233c8379441aadc4259faddfe229fcd9d39569
NicowaxDelux <nycko111995@gmail.com>
2024-05-31 14:02:11 -0500

    Develop a simple API using Python with the  module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleBaseHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        data = {"name": "John", "age": 30, "city": "New York"}
+        info = {"version": "1.0", "description": "A simple API built with http.server"}
+
+        if self.path == '/data':
+            try:
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                response_json = json.dumps(data)
+                self.wfile.write(response_json.encode('utf-8'))
+            except:
+                self.send_response(404)
+                self.send_header("Content-Type", "text/html")
+                self.end_headers()
+                self.wfile.write(b"404 Not Found")
+            finally:
+                return
+
+
+        if self.path == '/info':
+            try:
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                response_json = json.dumps(info)
+                self.wfile.write(response_json.encode('utf-8'))
+            except:
+                self.send_response(404)
+                self.send_header("Content-Type", "text/html")
+                self.end_headers()
+                self.wfile.write(b"404 Not Found")
+            finally:
+                return
+
+        try:
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        except:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+ 
+
+httpd = HTTPServer(('', 8000), SimpleBaseHTTPRequestHandler)
+httpd.serve_forever()
