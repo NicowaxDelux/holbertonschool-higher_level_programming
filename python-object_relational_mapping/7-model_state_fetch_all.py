f44a7160983515beea0951f323a220302d357927
NicowaxDelux <nycko111995@gmail.com>
2024-06-23 22:42:27 -0500

    lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
                     argv[1], argv[2], argv[3], pool_pre_ping=True))
     
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
-        print("{}: {}".format(instance.id, instance.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
     session.close()
