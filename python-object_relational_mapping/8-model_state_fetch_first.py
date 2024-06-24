558cbef55563d8bb0b7f9ba1115581c4222d5d03
NicowaxDelux <nycko111995@gmail.com>
2024-06-23 23:31:06 -0500

    prints the first State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,15 +11,14 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
                     argv[1], argv[2], argv[3], pool_pre_ping=True))
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
     if state is not None:
         print("{}: {}".format(state.id, state.name))
-
-    if state is None:
+    else:
         print("Nothing")
 
     session.close()
